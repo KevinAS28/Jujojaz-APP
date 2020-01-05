2deb235278c850aa465bd2d0c0ef5c086f679f41
Mulia16 <muliafirmansyah16@gmail.com>
2020-01-05 20:27:03 +0700

    Memperbarui activity add vehicle, Strings.xml, layout home dan menambahkan menu_add untuk activity add vehicle

--- App-Android Studio/JujojazBase/app/src/main/java/com/example/jujojazbase/AddVehicle.java
+++ App-Android Studio/JujojazBase/app/src/main/java/com/example/jujojazbase/AddVehicle.java
@@ -3,6 +3,7 @@ package com.example.jujojazbase;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
 
 public class AddVehicle extends AppCompatActivity {
 
@@ -10,5 +11,11 @@ public class AddVehicle extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_vehicle);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_add, menu);
+        return true;
     }
 }
