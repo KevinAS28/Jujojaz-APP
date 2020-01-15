8e0213093e024e7e3b0e5960cf1bdb1beffd52d3
Mulia16 <muliafirmansyah16@gmail.com>
2020-01-15 16:20:45 +0700

    Memperbarui activity home, strings.xml dan menambahkan drawable notification, search, dan menu home

--- App-Android Studio/JujojazBase/app/src/main/java/com/example/jujojazbase/Home.java
+++ App-Android Studio/JujojazBase/app/src/main/java/com/example/jujojazbase/Home.java
@@ -4,18 +4,25 @@ import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.util.Log;
+import android.view.Menu;
 import android.view.View;
 
 public class Home extends AppCompatActivity {
     FloatingActionButton fab;
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        toolbar = findViewById(R.id.homeToolBar);
+        setSupportActionBar(toolbar);
 
         fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -24,6 +31,12 @@ public class Home extends AppCompatActivity {
                 Log.d("Home", v.toString());
             }
         });
+
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_home, menu);
+        return true;
+    }
 }
