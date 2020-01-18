0570ed120996f45359f3b32c03759534a3cde9e1
kevin <kevinagusto28@gmail.com>
2020-01-18 15:27:07 +0700

    added add_vehicle to api view

--- Server_Django/API/views.py
+++ Server_Django/API/views.py
@@ -49,3 +49,32 @@ def edit_vehicle(request):
     kendaraan.save()
     return HttpResponse('')
 
+@jujojaz_login
+def add_vehicle(request):
+    user = User.objects.get(username=request.POST['username'])
+    merk_nama = request.POST["merk"]
+    merks = list(VehicleMerk.objects.filter(name=merk_nama))
+    if (len(merks)):
+        merk_object = merks[0]    
+    else:
+        merk_object = VehicleMerk(name=merk_nama)
+        merk_object.save()
+
+    type_nama = request.POST["tipe"]
+    types = list(VehicleType.objects.filter(name=type_nama))
+    if (len(types)):
+        type_object = types[0]    
+    else:
+        type_object = VehicleType(name=type_nama)
+        type_object.save()
+
+    tipe = type_object
+    merk = merk_object
+    pajak_setiap_berapa_hari = int(request.POST["pajak_setiap_berapa_hari"])
+    pajak_dimulai = request.POST["pajak_dimulai"]
+    servis_setiap_berapa_hari = int(request.POST["servis_setiap_berapa_hari"])
+    servis_dimulai = request.POST["servis_dimulai"]       
+    foto_foto = str(user.id)+'_'+str(Vehicle.objects.filter(user=user).order_by('id')[:1][0])
+    Vehicle(user=user, tipe=tipe, merk=merk, foto_foto=foto_foto, pajak_setiap_berapa_hari=pajak_setiap_berapa_hari, pajak_dimulai=pajak_dimulai, servis_setiap_berapa_hari=servis_setiap_berapa_hari, servis_dimulai=servis_dimulai).save()
+
+    return HttpResponse('')
