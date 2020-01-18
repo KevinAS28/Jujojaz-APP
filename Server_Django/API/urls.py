2b4451910fb688eea6d1ea4f97bb44702f9dfad5
kevin <kevinagusto28@gmail.com>
2020-01-18 15:27:40 +0700

    added urls in API

---
+++ Server_Django/API/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from .views import *
+
+app_name = 'API'
+
+urlpatterns = [
+    path('allvehicles/', get_all_vehicle),
+    path('addvehicle/', add_vehicle),
+    path('editvehicle/', edit_vehicle),
+    path('deletevehicle/', delete_vehicle)
+]
