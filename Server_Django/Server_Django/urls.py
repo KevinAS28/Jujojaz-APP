3af6229e53ed94a2ea6753652484d0dbb827e224
kevin <kevinagusto28@gmail.com>
2020-01-18 15:24:08 +0700

    Added API URL in main app

--- Server_Django/Server_Django/urls.py
+++ Server_Django/Server_Django/urls.py
@@ -14,11 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/token/', obtain_jwt_token),
-    path('api/token/verify/', verify_jwt_token),
-    path('api/token/refresh/', refresh_jwt_token),    
+    path('api/', include('API.urls', namespace='api')),
+    
 ]
