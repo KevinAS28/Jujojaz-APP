60e3adb2b6dfadda94f33bbb156117ef4bc9e784
kevin <kevinagusto28@gmail.com>
2020-01-21 15:16:31 +0700

    Auth adapted with imported class from MitraMeCash

--- App-Android Studio/JujojazBase/app/src/main/java/com/example/jujojazbase/Auth.java
+++ App-Android Studio/JujojazBase/app/src/main/java/com/example/jujojazbase/Auth.java
@@ -9,12 +9,26 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import json.JSONObject;
+
 
 public class Auth extends AppCompatActivity implements View.OnClickListener {
     EditText email, password;
     TextView signUp;
     Button signIn;
     Intent intent;
+
+    public void loginApiFail(){
+
+    }
+
+    public void loginApiSucceed(JSONObject data){
+
+    }
+
+    public void loginApiError(String msg){
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +43,41 @@ public class Auth extends AppCompatActivity implements View.OnClickListener {
 
         signIn = findViewById(R.id.btnSignIn);
         signIn.setOnClickListener(this);
+
+
+    }
+
+
+
+    public JSONObject loginApi(String username, String password, Boolean apiOnly){
+        JujojazLib net = new JujojazLib(){
+
+            @Override
+            public <List> void onDone(List x)  {
+
+                JSONObject data = new JSONObject(new String( Lib.Companion.Bytetobyte ((Byte[])((java.util.List<Byte>) x).toArray()) ));
+                if (data.get("success")=="true"){
+                    intent = new Intent(getApplicationContext(), HomeActivity.class);
+                    intent.putExtra("EMAIL", email.getText().toString().trim());
+                    startActivity(intent);
+                }
+                else{
+                    intent = new Intent(getApplicationContext(), EmailConfirmation.class);
+                    intent.putExtra("EMAIL", email.getText().toString().trim());
+                    startActivity(intent);
+                }
+
+            }
+
+
+        };
+        JSONObject authJson = new JSONObject();
+        authJson.put("username", username);
+        authJson.put("password", password);
+        return authJson;
+    }
+    public JSONObject loginApi(String username, String password){
+        return loginApi(username, password, true);
     }
 
     @Override
@@ -36,7 +85,7 @@ public class Auth extends AppCompatActivity implements View.OnClickListener {
         switch (v.getId()) {
             case R.id.btnSignIn :
                 Log.d("Auth", v.toString());
-                intent = new Intent(getApplicationContext(), Home.class);
+                intent = new Intent(getApplicationContext(), HomeActivity.class);
                 intent.putExtra("EMAIL", email.getText().toString().trim());
                 startActivity(intent);
                 break;
