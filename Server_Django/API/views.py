45f6b84d2a8dfc74e14a0cd8d274ef39adc523da
kevin <kevinagusto28@gmail.com>
2020-01-24 16:52:07 +0700

    Server json form adapted with android app

--- Server_Django/API/views.py
+++ Server_Django/API/views.py
@@ -2,20 +2,23 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from Server_Django.library import jujojaz_login
 from .models import *
-from django.http.response import HttpResponseNotAllowed
+from django.http.response import HttpResponseNotAllowed, JsonResponse
 from django.core import serializers
 from django.http import HttpResponse
 from django.forms import ModelForm
+import json
 # Create your views here.
 
 
 
 @jujojaz_login
 def get_all_vehicle(request):
-    user = User.objects.get(username=request.POST['username'])
+    user = User.objects.get(username=json.loads(request.POST['data'])['username'])
+    
     kendaraan = list(Vehicle.objects.filter(user=user))
-    kendaraan_json = serializers.serialize('json', kendaraan)
-    return HttpResponse(kendaraan_json, content_type='application/json')        
+    kendaraan_json = json.loads(serializers.serialize('json', kendaraan))
+    data = {"success": "true", "data": kendaraan_json}        
+    return JsonResponse(data)
 
 @jujojaz_login
 def edit_vehicle(request):
